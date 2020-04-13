9c2181222d726ef4d68d96633d99d669c7b4cd85
이성현 <dljdgus1211@naver.com>
2020-04-13 22:24:55 +0900

    Added image shift

--- ROI/ROI/Program.cs
+++ ROI/ROI/Program.cs
@@ -15,17 +15,29 @@ namespace ROI
             string imgPath = @"C:\Users\dljdg\Desktop\보내줄 사진\스크린샷(42).png";
 
             Mat src = new Mat(imgPath);
+            Mat srt = src.SubMat(new Rect(300, 300, 500, 500));
             Mat dst = src.SubMat(new Rect(300, 300, 500, 500));
 
             // 제로패딩 
-            Mat zeroPadding = new Mat();
+            Mat zeroPadding = new Mat(dst.Size(), dst.Type());
 
-            var M = Cv2.GetAffineTransform()
+            int shiftWidth = -100;
+            int shiftHeight = -100;
 
+            Rect srcRoi = new Rect(Math.Max(-shiftWidth, 0), Math.Max(-shiftHeight, 0), dst.Width - Math.Abs(shiftWidth), dst.Height - Math.Abs(shiftHeight));
+            Rect dstRoi = new Rect(Math.Max(shiftWidth, 0), Math.Max(shiftHeight, 0), dst.Width - Math.Abs(shiftWidth), dst.Height - Math.Abs(shiftHeight));
 
             Cv2.ImShow("src", src);
-            // Roi
+
+            // Roi 
+            Cv2.ImShow("str", srt);
+
+            // 제로 패딩
+            dst.SubMat(srcRoi).CopyTo(zeroPadding.SubMat(dstRoi));
+
             Cv2.ImShow("dst", dst);
+
+            Cv2.ImShow("zeroPadding", zeroPadding);
 
             Cv2.WaitKey(0);
         }
